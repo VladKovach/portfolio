51369ad10701d1e25763750f2e1ab3a269f60b5c
Vlad Kovach <vladkovach00@gmail.com>
2025-04-30 20:13:18 +0200

    fix scroll

--- src/App.jsx
+++ src/App.jsx
@@ -13,19 +13,19 @@ function App() {
     <SideNav />
       <div className="flex-1 ml-24 mr-4 max-sm:ml-14">
         <main className="max-w-[1100px] mx-auto pb-20">
-          <section id="about" className="pt-16 scroll-mt-20">
+          <section id="about" className="pt-16 scroll-mt-10">
             <Hero />
           </section>
-          <section id="skills" className="pt-16 max-sm:pt-0 scroll-mt-20">
+          <section id="skills" className="pt-16 max-sm:pt-0 ">
             <Skills />
           </section>
-          <section id="projects" className="pt-16 scroll-mt-20">
+          <section id="projects" className="pt-16 ">
             <Projects />
           </section>
-          <section id="experience" className="pt-16 scroll-mt-20">
+          <section id="experience" className="pt-16 ">
             <Experience />
           </section>
-          <section id="contact" className="pt-16 scroll-mt-20">
+          <section id="contact" className="pt-16 ">
             <Contact />
           </section>
         </main>
