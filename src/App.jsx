f910dbb4c99bb0490ebac01eca3eaaca9d326cd3
Vlad Kovach <vladkovach00@gmail.com>
2025-05-07 22:06:45 +0200

    add browserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,46 @@ import Projects from "./components/Projects/Projects";
 import SideNav from "./components/SideNav";
 import Contact from "./components/Contact/Contact";
 import ReactGA from "./ga";
+import { useLocation } from "react-router-dom";
 
 function App() {
-  ReactGA.send({
-    hitType: "pageview",
-    page: window.location.pathname,
-  });
+  function AnalyticsTracker() {
+    const location = useLocation();
+
+    useEffect(() => {
+      ReactGA.send({ hitType: "pageview", page: location.pathname });
+    }, [location]);
+
+    return null;
+  }
   return (
-    <div className="h-full relative">
-      <Header />
-      <SideNav />
-      <div className="flex-1 ml-24 mr-4 max-sm:ml-14">
-        <main className="max-w-[1100px] mx-auto pb-20">
-          <section id="about" className="pt-16 scroll-mt-10">
-            <Hero />
-          </section>
-          <section id="skills" className="pt-16 max-sm:pt-0 ">
-            <Skills />
-          </section>
-          <section id="projects" className="pt-16 ">
-            <Projects />
-          </section>
-          <section id="experience" className="pt-16 ">
-            <Experience />
-          </section>
-          <section id="contact" className="pt-16 ">
-            <Contact />
-          </section>
-        </main>
+    <BrowserRouter>
+      <AnalyticsTracker />
+
+      <div className="h-full relative">
+        <Header />
+        <SideNav />
+        <div className="flex-1 ml-24 mr-4 max-sm:ml-14">
+          <main className="max-w-[1100px] mx-auto pb-20">
+            <section id="about" className="pt-16 scroll-mt-10">
+              <Hero />
+            </section>
+            <section id="skills" className="pt-16 max-sm:pt-0 ">
+              <Skills />
+            </section>
+            <section id="projects" className="pt-16 ">
+              <Projects />
+            </section>
+            <section id="experience" className="pt-16 ">
+              <Experience />
+            </section>
+            <section id="contact" className="pt-16 ">
+              <Contact />
+            </section>
+          </main>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
